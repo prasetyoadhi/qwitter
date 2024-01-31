8629a9cd86c8c34d957fdf2b64ebb417b39d8da4
Adhi <adhiprst@gmail.com>
2024-01-31 18:01:12 +0700

    initial commit

---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        name: "Home",
+        path: "",
+        component: () => import("src/pages/PageHome.vue"),
+      },
+      {
+        name: "About",
+        path: "/about",
+        component: () => import("src/pages/PageAbout.vue"),
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
